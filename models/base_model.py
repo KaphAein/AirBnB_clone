f228887f34c6a86f31986a1c873028d77072aed8
kaphaein <cherifafadaly@gmail.com>
2024-03-11 04:33:54 -0700

    “Base_Model_and_FIle_Storage”

--- models/base_model.py
+++ models/base_model.py
@@ -25,28 +25,28 @@ class BaseModel():
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
             models.storage.new(self)
+
+    def __str__(self):
+        '''print: [<class name>] (<self.id>) <self.__dict__>'''
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def __repr__(self):
         '''returns string repr'''
         return (self.__str__())
 
-    def __str__(self):
-        '''print: [<class name>] (<self.id>) <self.__dict__>'''
-        return "[{}] ({}) {}".format(
-            type(self).__name__, self.id, self.__dict__)
-
     def save(self):
         '''updates the updated_at with the current datetime'''
-        self.updated_at = datetime.utcnow()
+        self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         '''returns a dictionary containing all keys/values of __dict__'''
         ret_dict = {**self.__dict__}
-        ret_dict['__class__'] = type(self).__name__
+        ret_dict['__class__'] = self.__class__.__name__
         ret_dict['created_at'] = self.created_at.isoformat()
         ret_dict['updated_at'] = self.updated_at.isoformat()
         return ret_dict
