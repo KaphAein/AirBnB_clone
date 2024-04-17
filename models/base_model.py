1471f240fff04c36a17f220fa036ea913da23b42
kaphaein <cherifafadaly@gmail.com>
2024-04-17 15:20:43 -0700

    AIRBNB

--- models/base_model.py
+++ models/base_model.py
@@ -1,50 +1,75 @@
 #!/usr/bin/python3
+"""
+Contains class BaseModel
+"""
 
-'''BaseModel that defines all common attributes/methods for other classes'''
-from uuid import uuid4
 from datetime import datetime
 import models
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import uuid
+
+time = "%Y-%m-%dT%H:%M:%S.%f"
+
+if models.storage_t == "db":
+    Base = declarative_base()
+else:
+    Base = object
 
 
-class BaseModel():
-    '''BaseModel Class for all classes'''
+class BaseModel:
+    """The BaseModel class from which future classes will be derived"""
+    if models.storage_t == "db":
+        id = Column(String(60), primary_key=True)
+        created_at = Column(DateTime, default=datetime.utcnow)
+        updated_at = Column(DateTime, default=datetime.utcnow)
 
     def __init__(self, *args, **kwargs):
-        '''serialization and deserialization of class'''
+        """Initialization of the base model"""
         if kwargs:
             for key, value in kwargs.items():
-                if 'created_at' == key:
-                    self.created_at = datetime.strptime(
-                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                elif 'updated_at' == key:
-                    self.updated_at = datetime.strptime(
-                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-                elif '__class__' != key:
+                if key != "__class__":
                     setattr(self, key, value)
+            if kwargs.get("created_at", None) and type(self.created_at) is str:
+                self.created_at = datetime.strptime(kwargs["created_at"], time)
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and type(self.updated_at) is str:
+                self.updated_at = datetime.strptime(kwargs["updated_at"], time)
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
         else:
-            self.id = str(uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.utcnow()
+            self.updated_at = self.created_at
 
     def __str__(self):
-        '''print: [<class name>] (<self.id>) <self.__dict__>'''
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
-
-    def __repr__(self):
-        '''returns string repr'''
-        return (self.__str__())
+        """String representation of the BaseModel class"""
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
+                                         self.__dict__)
 
     def save(self):
-        '''updates the updated_at with the current datetime'''
-        self.updated_at = datetime.now()
+        """updates the attribute 'updated_at' with the current datetime"""
+        self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
-        '''returns a dictionary containing all keys/values of __dict__'''
-        ret_dict = {**self.__dict__}
-        ret_dict['__class__'] = self.__class__.__name__
-        ret_dict['created_at'] = self.created_at.isoformat()
-        ret_dict['updated_at'] = self.updated_at.isoformat()
-        return ret_dict
+        """returns a dictionary containing all keys/values of the instance"""
+        new_dict = self.__dict__.copy()
+        if "created_at" in new_dict:
+            new_dict["created_at"] = new_dict["created_at"].strftime(time)
+        if "updated_at" in new_dict:
+            new_dict["updated_at"] = new_dict["updated_at"].strftime(time)
+        new_dict["__class__"] = self.__class__.__name__
+        if "_sa_instance_state" in new_dict:
+            del new_dict["_sa_instance_state"]
+        return new_dict
+
+    def delete(self):
+        """delete the current instance from the storage"""
+        models.storage.delete(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,55 +1,70 @@
 #!/usr/bin/python3
-'''
-serializes instances to a JSON file
-and deserializes JSON file to instances
-'''
+"""
+Contains the FileStorage class
+"""
 
 import json
-from models.base_model import BaseModel
-from json.decoder import JSONDecodeError
-from models.user import User
-from models.state import State
-from models.city import City
 from models.amenity import Amenity
+from models.base_model import BaseModel
+from models.city import City
 from models.place import Place
 from models.review import Review
+from models.state import State
+from models.user import User
+
+classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
 
 class FileStorage:
-    '''
-    Object relation mapping to interface or database
-    '''
-    __file_path = 'file.json'
-    __objects = {}
-    class_dict = {"BaseModel": BaseModel, "User": User, "Place": Place,
-                  "Amenity": Amenity, "City": City, "Review": Review,
-                  "State": State}
+    """serializes instances to a JSON file & deserializes back to instances"""
 
-    def all(self):
-        '''returns the dictionary __objects'''
+    # string - path to the JSON file
+    __file_path = "file.json"
+    # dictionary - empty but will store all objects by <class name>.id
+    __objects = {}
+
+    def all(self, cls=None):
+        """returns the dictionary __objects"""
+        if cls is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if cls == value.__class__ or cls == value.__class__.__name__:
+                    new_dict[key] = value
+            return new_dict
         return self.__objects
 
     def new(self, obj):
-        '''sets in __objects the obj with key <obj class name>.id'''
-        if obj:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        """sets in __objects the obj with key <obj class name>.id"""
+        if obj is not None:
+            key = obj.__class__.__name__ + "." + obj.id
             self.__objects[key] = obj
 
     def save(self):
-        '''serializes __objects to the JSON file (path: __file_path)'''
-        ser = {}
-        for key, val in self.__objects.items():
-            ser[key] = val.to_dict()
-        with open(self.__file_path, 'w', encoding="UTF-8") as f:
-            json.dump(ser, f)
+        """serializes __objects to the JSON file (path: __file_path)"""
+        json_objects = {}
+        for key in self.__objects:
+            json_objects[key] = self.__objects[key].to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(json_objects, f)
 
     def reload(self):
-        '''deserializes the JSON file to __objects (only if the JSON file'''
+        """deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r', encoding="UTF-8") as f:
-                des = json.load(f)
-            for key, val in des.items():
-                obj = self.class_dict[val['__class__']](**val)
-                self.__objects[key] = obj
-        except (FileNotFoundError, JSONDecodeError):
+            with open(self.__file_path, 'r') as f:
+                jo = json.load(f)
+            for key in jo:
+                self.__objects[key] = classes[jo[key]["__class__"]](**jo[key])
+        except:
             pass
+
+    def delete(self, obj=None):
+        """delete obj from __objects if it’s inside"""
+        if obj is not None:
+            key = obj.__class__.__name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
